aca67ed6bc2bc9d8e623bf0de2ded78dcc9bada1
jayson-jung <hyeonsuns@kakao.com>
2022-02-20 15:24:08 +0900

    Interface 를 활용한 다양한 예제 실습

--- src/chapter10/interfaceex/Calc.java
+++ src/chapter10/interfaceex/Calc.java
@@ -8,4 +8,15 @@ public interface Calc {// 인터페이스엔 상수와 추상메서드가 정의
     int subtract(int num1, int num2);
     int times(int num1, int num2);
     int divide(int num1, int num2);
+
+    default void description() {    //공통적으로 사용되는 구현데이터가 있을경우 default 메서드로 정의한다.
+        System.out.println("정수 계산기를 구현합니다.");
+    }
+
+    static  int total(int[] arr) {
+        int total = 0;
+
+        for(int i : arr) total += i;
+        return total;
+    }
 }
--- src/chapter10/interfaceex/CalculatorTest.java
+++ src/chapter10/interfaceex/CalculatorTest.java
@@ -9,5 +9,11 @@ public class CalculatorTest {
         Calc calc = new CompleteCalc(); //인터페이스를 기반으로 구현된 인스턴스클래스를 가져다 쓰는걸 클라이언트코드라고 부른다.(서비스를 가져다쓰는쪽)
 
         System.out.println(calc.add(num1, num2));
+
+        calc.description();
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = Calc.total(arr);
+        System.out.println(sum);
     }
 }
